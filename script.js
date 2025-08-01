7c316cebe8caf78d67d76cd2bf1ad4a3c92c0048
Ivan Castellanos <ivanca@gmail.com>
2025-07-31 19:00:52 -0500

    Cleaning JS

--- script.js
+++ script.js
@@ -1,32 +1,4 @@
-/*
-// from:
 
-.ScrollableItem_item__mXfrR {
-    max-width: 58.8235294118%;
-    flex-basis: 58.8235294118%;
-}
-
-.c-lot-card__image {
-    position: relative;
-    overflow: hidden;
-    padding-top: 125%
-}
-
-// to: 
-
-.ScrollableItem_item__mXfrR {
-    max-width: 91.8235294118% !important;
-    flex-basis: 91.8235294118% !important;
-}
-
-.c-lot-card__image {
-    position: relative !important;
-    overflow: hidden !important;
-    padding-top: 155% !important;
-}
-*/
-let mouseDown = false;
-let startX, scrollLeft;
 const pointerScroll = (elem) => {
     let percentageTransitioned = 0;
     let locked = false;
@@ -34,14 +6,23 @@ const pointerScroll = (elem) => {
     let flexDiff = 81.82 - flexBasis;
     let paddingTop = 125;
     let paddingDiff = 30;
+    let scrollStart = -1;
+    let scrollDiff = -1;
     const scrollableItems = elem.querySelectorAll('.ScrollableItem_item__mXfrR');
     const cardImages = elem.querySelectorAll('.c-lot-card__image');
-    console.log(scrollableItems, cardImages)
-    const dragStart = (ev) => elem.setPointerCapture(ev.pointerId);
-    const dragEnd = (ev) => elem.releasePointerCapture(ev.pointerId);
+    const content = document.querySelector('.content1');
+    const navBar = document.querySelector('.FeedAndCategoryNavigation_container__9UnwL');
+    const dragStart = (ev) => {
+        elem.setPointerCapture(ev.pointerId);
+        scrollStart = content.scrollTop;
+        scrollDiff = elem.parentElement.getBoundingClientRect().top - navBar.getBoundingClientRect().bottom;
+        document.body.style.cursor = 'grabbing';
+    };
+    const dragEnd = (ev) => {
+        elem.releasePointerCapture(ev.pointerId);
+        document.body.style.cursor = '';
+    }
     const drag = (ev) => {
-
-
         if (elem.hasPointerCapture(ev.pointerId)) {
             elem.scrollLeft -= ev.movementX;
             if (!locked) {
@@ -54,20 +35,17 @@ const pointerScroll = (elem) => {
                 }
                 let newFlexBasis = flexBasis + flexDiff * f;
                 let newPaddingTop = paddingTop + paddingDiff * f;
+                content.scrollTop = scrollStart + scrollDiff * f;
                 cardImages.forEach(e => {
                     e.style.paddingTop = newPaddingTop + '%';
-                    // console.log(e, e.style.paddingTop);
                 });
                 scrollableItems.forEach(e => {
                     e.style.flexBasis = newFlexBasis + '%';
                     e.style.maxWidth = e.style.flexBasis;
-                    // console.log(e, e.style.flexBasis)
                 });
-                console.log(newFlexBasis, newPaddingTop)
             }
         }
     };
-
     elem.addEventListener("pointerdown", dragStart);
     elem.addEventListener("pointerup", dragEnd);
     elem.addEventListener("pointermove", drag);
